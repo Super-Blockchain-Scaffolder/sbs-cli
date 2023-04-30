24206f3ed11b37c52e2ea03927675be947f2e4b5
JimLynchCodes <mrdotjim@gmail.com>
2023-04-30 19:41:48 -0400

    getting master list data

--- src/data_reading/master_list_reader.rs
+++ src/data_reading/master_list_reader.rs
@@ -1,1 +1,14 @@
-// TODO read_master_list() 
+use serde_yaml::Value;
+use std::error::Error;
+
+use crate::shared_constants::URL;
+use reqwest::blocking::get;
+
+const master_list_url = "https://raw.githubusercontent.com/Blockchain-Super-Scaffolder/bss-master-list/main/bss-master-list.yaml";
+
+pub fn get_master_list_data() -> Result<(), Box<dyn Error>> {
+    let response = get(URL)?.text()?;
+    let serde_result = serde_yaml::from_str::<Value>(&response)?;
+    Ok(serde_result)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ fn main() {
 
     let full_args = prompt_for_needed_data(args_passed_in);
 
-    let starter_data = get_starter_data_from_name(full_args.starter_template);
+    let master_list_data = get_full_master_list()
+
+    let starter_data = get_starter_data_from_list(full_args.starter_template, master_list_data);
 
     if full_args.current_directory {
-        git_cloner.clone_repo_in_current_dir(starter_data);
+        git_cloner.clone_repo_in_current_dir(starter_data.repo_url);
     } else {
         git_cloner.clone_repo_in_named_dir(starter_data, dir_name);
     }
