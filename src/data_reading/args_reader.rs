e052a8a03e84b8d958a63e0f725fc5711e3cba42
JimLynchCodes <mrdotjim@gmail.com>
2023-04-30 19:33:57 -0400

    initial scratch code in main

--- src/data_reading/args_reader.rs
+++ src/data_reading/args_reader.rs
@@ -1,1 +1,18 @@
-// TODO read_master_list() 
+use clap::Parser;
+
+#[derive(Parser)]
+pub struct Cli {
+
+    #[arg(short, long)]
+    pub current_directory: Option<bool>,
+
+    #[arg(short, long)]
+    pub named_directory: Option<String>,
+
+    #[arg(short, long)]
+    pub starter_template: Option<String>,
+}
+
+pub fn read_args() -> Cli {
+    Cli::parse()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+
+    let args_passed_in = read_args();
+
+    validate_args(&args_passed_in);
+
+    let full_args = prompt_for_needed_data(args_passed_in);
+
+    let starter_data = get_starter_data_from_name(full_args.starter_template);
+
+    if full_args.current_directory {
+        git_cloner.clone_repo_in_current_dir(starter_data);
+    } else {
+        git_cloner.clone_repo_in_named_dir(starter_data, dir_name);
+    }
+
+    git_deleter.delete_git_folder()
+
+    println!("Successfully scaffolded!");
 }
