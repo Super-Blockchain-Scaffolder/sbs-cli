7658017ada9c8d4c29ef6f4f139017765d02039d
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-05-21 02:03:41 -0400

    release: build v0.0.3

--- pre-install.js
+++ pre-install.js
@@ -29,8 +29,8 @@ if (fs.existsSync(cargoDir)) {
     
 const features = process.env.npm_config_features ? `--features ${process.env.npm_config_features.replace(",", " ")}` : ""; 
 
-console.log(`Installing and compiling sbs 0.0.2 ${features} ...`);
-exec(`cargo install sbs --vers 0.0.2 ${features}`, (error, stdout, stderr) => {
+console.log(`Installing and compiling sbs 0.0.3 ${features} ...`);
+exec(`cargo install sbs --vers 0.0.3 ${features}`, (error, stdout, stderr) => {
   console.log(stdout);
   if (error || stderr) {
     console.log(error || stderr);
